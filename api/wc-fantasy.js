bc830840ca24f500162ec6a8be92d31a414023aa
Jonno <jonno@m1llar.com>
2026-06-15 12:53:44 +0100

    Reduce WC fantasy cache TTL so cycling stats panel deploys propagate faster
    The cycling goals/assists/value panel was already deployed but stale
    CDN responses (s-maxage=3600) kept serving the old price-leaders shape
    for up to an hour. Match the 5-min TTL used elsewhere.

--- api/wc-fantasy.js
+++ api/wc-fantasy.js
@@ -36,7 +36,7 @@ async function fetchTopStats() {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=7200');
+  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate=600');
 
   try {
     const [playersRaw, squadsRaw, roundsRaw, topStats] = await Promise.all([
